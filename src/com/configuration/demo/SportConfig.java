8f62d29b1bb552ada3fefe3ff3343437bac19314
WillyLu326 <luzh0326@gmail.com>
2018-03-14 19:21:30 -0400

    add @ComponentScan annotation

--- src/com/configuration/demo/SportConfig.java
+++ src/com/configuration/demo/SportConfig.java
@@ -1,8 +1,10 @@
 package com.configuration.demo;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan("com.configuration.demo")
 public class SportConfig {
 
 }
