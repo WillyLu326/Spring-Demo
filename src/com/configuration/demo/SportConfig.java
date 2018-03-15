59812ab187064c5f28b015fb31f86253c4ddc647
WillyLu326 <luzh0326@gmail.com>
2018-03-14 20:32:27 -0400

    Spring Configuration with Java Code

--- src/com/configuration/demo/SportConfig.java
+++ src/com/configuration/demo/SportConfig.java
@@ -2,9 +2,11 @@ package com.configuration.demo;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @ComponentScan("com.configuration.demo")
+@PropertySource("classpath:sport.properties")
 public class SportConfig {
 
 }
